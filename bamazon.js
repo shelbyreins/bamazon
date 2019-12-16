50b3db91a8cd45b6edb8947b5cd0756f1c89209e
Shelby Reinsimar <shelbyreins@yahoo.com>
2019-12-15 19:30:31 -0800

    Add in restart and quit functions

--- bamazon.js
+++ bamazon.js
@@ -39,39 +39,96 @@ function messages() {
     inquirer
         .prompt([
             {
-                name: "ID",
+                name: "item_id",
                 type: "input",
                 message: "What is the ID of the item you would like to purchase?",
                 filter: Number
             },
             {
-                name: "Quanity",
+                name: "stock_quanity",
                 type: "input",
                 message: "How many item(s) would you like to purchase?",
                 filter: Number
             }
         ])
         .then(function (answers) {
-            ordering();
+            var inputId = answers.item_id;
+            var quantity = answers.stock_quanity;
+            connection.query("SELECT * FROM products WHERE item_id = " + inputId, function (err, response) {
+                if (err) throw err;
+                if(answers === "Q"){
+                    quit();
+                }
+                if (response[0].stock_quanity >= quantity) {
+                    console.log("Your order has been completed.")
+
+                    connection.query("UPDATE products SET stock_quanity =" + (response[0].stock_quanity - quantity) + " WHERE item_id =" + inputId, function (error, response) {
+                        if (error) throw error;
+                        console.log("||||||||||||||||||||||||||||||||||||")
+                        console.log("Your order has been placed!")
+                        console.log("||||||||||||||||||||||||||||||||||||")
+
+                        restart();
+
+                    })
+
+
+
+                } else {
+                    console.log("||||||||||||||||||||||||||||||||||||")
+                    console.log("Please modify your order.")
+                    console.log("Sorry, there is not enough product in stock.")
+                    console.log("||||||||||||||||||||||||||||||||||||")
+
+                    modifyOrder();
+                }
+
+            });
         });
 }
 
-function ordering() {
-
-    var ID = answers.ID;
-    var quantity = answers.Quantity;
-    connection.query("SELECT * FROM products WHERE ID=" + item_id, function (err, response) {
-        if (err) throw err;
-            if (response[0].stock_quanity - quantity == 0){
-                console.log("Your order has been completed.")
-
-                // connection.query("UPDATE products SET stock_quanity = ? WHERE id = ?",
-                // [
-                //     response[i].stock_quanity - quantity
-                // ]
-                // )
+function restart() {
+    inquirer
+        .prompt([
+            {
+                name: "restart",
+                type: "list",
+                message: "Do you want to shop more?",
+                choices: ["Yes", "No"]
+            },
+        ])
+        .then(function (answer) {
+            if (answer.modifyOrder === "Yes") {
+                display()
+            } else {
+                quit()
             }
+        });
 
+}
+
+function modifyOrder(){
+    inquirer
+    .prompt([
+        {
+            name: "modifyOrder",
+            type: "list",
+            message: "Would you like to modify your order?",
+            choices: ["Yes", "No"]
+        },
+    ])
+    .then(function (answer) {
+        if (answer.modifyOrder === "Yes")  {
+            display();
+        } else {
+            quit();
+        }
     });
 
+}
+function quit() {
+    console.log("||||||||||||||||||||||||||||||||||||");
+    console.log("Thanks for visiting, see you soon!");
+    console.log("||||||||||||||||||||||||||||||||||||");
+    connection.end();
 }
